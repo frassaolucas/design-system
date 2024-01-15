b6e9727df7ba508e67bb0c6e398d99fe83130086
Lucas Frassão <frassao.lucas@gmail.com>
2024-01-15 14:47:51 -0300

    feature: created avatar component and it's doc

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://picsum.photos/id/64/500',
+    alt: 'Avatar photo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
