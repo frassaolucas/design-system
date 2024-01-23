e0d5ff0e87e69b8dd9f8eb8d6ae16302f7aff0fb
Lucas Frassão <frassao.lucas@gmail.com>
2024-01-23 08:32:33 -0300

    fix: ad subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -24,6 +24,14 @@ module.exports = {
 
   docs: {
     autodocs: true
+  },
+
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+
+    return config
   }
 }
 
